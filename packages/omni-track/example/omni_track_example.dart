520aac814ae40ca4801d4c77a9dfa1b22122b27e
Rodrigo Rahman <rodrigorahman@gmail.com>
2025-02-20 01:45:34 -0300

    fix: Recreate Flutter project correctly on omni-track

--- packages/omni-track/example/omni_track_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:omni_track/omni_track.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- packages/omni-track/lib/omni_track.dart
+++ packages/omni-track/lib/omni_track.dart
@@ -1,8 +1,5 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library;
-
-export 'src/omni_track_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
